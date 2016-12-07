964bd6ddf717c58751e475b2c1078a72ab51418e
Doryeong Park <zzada0520@gmail.com>
2016-12-07 20:12:35 +0900

    Prototype1 C# code is updated

---
+++ Prototype#1/Prototype(C#)/Prototype/PrototypeForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Prototype
+{
+    partial class PrototypeForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picb_original = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.picb_result = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.lbl_motionnotifier = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picb_original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picb_result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picb_original
+            // 
+            this.picb_original.Location = new System.Drawing.Point(52, 105);
+            this.picb_original.Name = "picb_original";
+            this.picb_original.Size = new System.Drawing.Size(611, 457);
+            this.picb_original.TabIndex = 0;
+            this.picb_original.TabStop = false;
+            // 
+            // picb_result
+            // 
+            this.picb_result.Location = new System.Drawing.Point(700, 105);
+            this.picb_result.Name = "picb_result";
+            this.picb_result.Size = new System.Drawing.Size(611, 457);
+            this.picb_result.TabIndex = 1;
+            this.picb_result.TabStop = false;
+            // 
+            // lbl_motionnotifier
+            // 
+            this.lbl_motionnotifier.AutoSize = true;
+            this.lbl_motionnotifier.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbl_motionnotifier.ForeColor = System.Drawing.Color.Lime;
+            this.lbl_motionnotifier.Location = new System.Drawing.Point(54, 44);
+            this.lbl_motionnotifier.Name = "lbl_motionnotifier";
+            this.lbl_motionnotifier.Size = new System.Drawing.Size(0, 41);
+            this.lbl_motionnotifier.TabIndex = 2;
+            this.lbl_motionnotifier.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PrototypeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 605);
+            this.Controls.Add(this.lbl_motionnotifier);
+            this.Controls.Add(this.picb_result);
+            this.Controls.Add(this.picb_original);
+            this.Name = "PrototypeForm";
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnDestroy);
+            this.Load += new System.EventHandler(this.OnLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.picb_original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picb_result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl picb_original;
+        private OpenCvSharp.UserInterface.PictureBoxIpl picb_result;
+        private System.Windows.Forms.Label lbl_motionnotifier;
+    }
+}
+
---
+++ Prototype#1/Prototype(C#)/Prototype/PrototypeForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MetroFramework;
+using MetroFramework.Forms;
+
+using OpenCvSharp;
+using System.Threading;
+
+/*
+ * Prototype#1
+ * 
+ * Author   :   Doryeong Park
+ * Date     :   7. 12. 2016
+ * Desc     :   This code is for test of simple motion detection algorithm with C#
+ */
+namespace Prototype
+{
+    public partial class PrototypeForm : MetroForm
+    {
+
+        private Thread frameRunner;
+        private VideoCapture camera;
+        private Mat currentFrame;
+        private Mat previousFrame;
+        private Mat resultFrame;
+        private Mat posterizedFrame;
+
+        private const int GCCOUNT = 30;
+
+        private int motionIndex;
+      
+        public PrototypeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            currentFrame = new Mat();
+            posterizedFrame = new Mat();
+            previousFrame = new Mat();
+
+            frameRunner = new Thread(new ThreadStart(delegate () { RunFrame(); }));
+            frameRunner.Start();
+        }
+
+
+        private void RunFrame()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            var gcCounter = 0;
+
+            //Camera from webcam
+            camera = new VideoCapture(0);
+
+            while (true)
+            {
+                //Grab 1 frame
+                camera.Read(currentFrame);
+                posterizedFrame = new Mat();
+                picb_original.ImageIpl = currentFrame;
+
+                Cv2.CvtColor(currentFrame, posterizedFrame, ColorConversionCodes.BGR2GRAY);
+
+                //8 Level Posterizing
+                EightLevelPosterizing();
+
+                //Subtract Images
+                resultFrame = posterizedFrame - previousFrame;
+
+                motionIndex = 0;
+                var resultFrameIndexer = resultFrame.GetGenericIndexer<byte>();
+
+                //Remove noises simply
+                for (int i = 0; i < resultFrame.Height; ++i)
+                {
+                    for (int j = 0; j < resultFrame.Width; ++j)
+                    {
+                        var currentPixel = resultFrameIndexer[i, j];
+                        if (currentPixel <= 96)
+                            resultFrameIndexer[i, j] = 0;
+                        else
+                            ++motionIndex;
+                    }
+                }
+
+                //This value can be sensitivity
+                if (motionIndex >= 100)
+                {
+                    lbl_motionnotifier.Text = "Motion Detected";
+                }
+                else
+                {
+                    lbl_motionnotifier.Text = "";
+                }
+
+                picb_result.ImageIpl = resultFrame;
+                previousFrame = posterizedFrame;
+
+                //Compel garbage collector collect memory of image
+                if(GCCOUNT == gcCounter++)
+                {
+                    gcCounter = 0;
+                    GC.Collect();
+                }
+                
+                var ch = Cv2.WaitKey(1);
+                if (ch == 27)
+                    break;
+            }
+        }
+        
+        /* Mapping values lowest value of each 8 ranges */
+        private void EightLevelPosterizing()
+        {
+            var frameIndexer = posterizedFrame.GetGenericIndexer<byte>();
+            for (int i = 0; i < posterizedFrame.Height; ++i)
+                for (int j = 0; j < posterizedFrame.Width; ++j)
+                {
+                    var currentPixel = frameIndexer[i, j];
+                    if (0 <= currentPixel && currentPixel <= 32)
+                        frameIndexer[i, j] = 0;
+                    else if (32 < currentPixel && currentPixel <= 64)
+                        frameIndexer[i, j] = 32;
+                    else if (64 < currentPixel && currentPixel <= 96)
+                        frameIndexer[i, j] = 64;
+                    else if (96 < currentPixel && currentPixel <= 128)
+                        frameIndexer[i, j] = 96;
+                    else if (128 < currentPixel && currentPixel <= 160)
+                        frameIndexer[i, j] = 128;
+                    else if (160 < currentPixel && currentPixel <= 192)
+                        frameIndexer[i, j] = 160;
+                    else if (192 < currentPixel && currentPixel <= 224)
+                        frameIndexer[i, j] = 192;
+                    else
+                        frameIndexer[i, j] = 224;
+                }
+        }
+
+        private void OnDestroy(object sender, FormClosedEventArgs e)
+        {
+            camera.Dispose();
+        }
+    }
+}
